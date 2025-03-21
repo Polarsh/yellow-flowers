7d5826bf48a9dd1b71b0d6a04e55f73d28a3c827
Yorch Sanchez <dybsm98@outlook.es>
2025-03-21 11:29:47 -0500

    Personalizacion de nombre

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
+
+  function getNameFromUrl() {
+    const url = window.location.pathname;
+    const name = url.substring(url.lastIndexOf("/") + 1);
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    return name;
+  }
+
+  const name = getNameFromUrl();
+
+  if (name) {
+    const personalizedMessage = `¡Aquí tienes un ramo de flores para ti, ${name}! 🌻🌼💛`;
+    const personalizedGreeting = `¡Hola, ${name}!`;
+
+    document.getElementById("greeting").textContent = personalizedGreeting;
+    document.getElementById("message").textContent = personalizedMessage;
+
+    document.title = `${name}'s Flowers - Un regalo especial`;
+    document
+      .querySelector("meta[property='og:title']")
+      .setAttribute("content", `${name}'s Flowers - Un regalo especial`);
+    document
+      .querySelector("meta[property='og:description']")
+      .setAttribute(
+        "content",
+        `Un ramo especial para ti, ${name}! Disfruta estas flores.`
+      );
+    document
+      .querySelector("meta[property='og:image']")
+      .setAttribute("content", "/public/icon.png");
+    document
+      .querySelector("meta[property='og:url']")
+      .setAttribute("content", window.location.href);
+  }
 };
